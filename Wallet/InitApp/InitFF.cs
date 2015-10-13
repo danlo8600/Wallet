2427ed25ef97f11ebe93d50bec3537521aafddc8
Danilo <md_darkangel86@hotmail.com>
2015-10-13 17:10:14 +0200

    Some changes to file name

---
+++ Wallet/InitApp/InitFF.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Wallet.InitApp
+{
+    class InitFF
+    {
+        private StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+        private StorageFolder appFolder;
+
+        private String main_folder = "wallet";
+        private String db_file = "db_wallet";
+
+        public String error = null;
+
+        public InitFF()
+        {
+            CreateFF();
+        }
+
+        private bool createDB()
+        {
+
+            return true;
+        }
+
+        //Old but gold
+        private async Task<bool> CreateFF()
+        {
+            //Create app folder and file into AppData
+            try
+            {
+                storageFolder.CreateFolderAsync(main_folder);
+                appFolder = await storageFolder.GetFolderAsync(main_folder);
+                appFolder.CreateFileAsync(db_file);
+            }
+            catch (System.IO.FileNotFoundException fn)
+            {
+                error = fn.Message;
+                return false;
+            }
+            catch (System.UnauthorizedAccessException ua)
+            {
+                error = ua.Message;
+                return false;
+            }
+            catch (System.ArgumentException ae)
+            {
+                error = ae.Message;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Wallet/MainPage.xaml.cs
+++ Wallet/MainPage.xaml.cs
@@ -12,8 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.Storage;
-using System.Threading.Tasks;
+
 
 // Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
 
@@ -24,47 +23,10 @@ namespace Wallet
     /// </summary>
     public sealed partial class MainPage : Page
     {
-
-        private StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
-        private StorageFolder appFolder;
-
-        private String main_folder = "wallet";
-        private String db_file = "fist";
-
-        public String error = null;
-
         public MainPage()
         {
+            new InitApp.InitFF();
             this.InitializeComponent();
-            InitAppFile();
-        }
-
-        async Task<bool> InitAppFile()
-        {
-            //Create app folder and file into AppData
-            try
-            {
-                storageFolder.CreateFolderAsync(main_folder);
-                appFolder = await storageFolder.GetFolderAsync(main_folder);
-                appFolder.CreateFileAsync(db_file);
-            }
-            catch (System.IO.FileNotFoundException fn)
-            {
-                error = fn.Message;
-                return false;
-            }
-            catch (System.UnauthorizedAccessException ua)
-            {
-                error = ua.Message;
-                return false;
-            }
-            catch (System.ArgumentException ae)
-            {
-                error = ae.Message;
-                return false;
-            }
-
-            return true;
         }
     }
 }
