a1a27501bcecafbd7a5babd3d1e931e5ed6fc6b1
Danilo <md_darkangel86@hotmail.com>
2015-10-29 10:37:33 +0100

    Remove some inused import

--- Wallet/MainPage.xaml.cs
+++ Wallet/MainPage.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Windows.Storage;
 using Wallet.DbManager;
-using System.Globalization;
-using System.Diagnostics;
 using System.Collections.ObjectModel;
 // Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
 
